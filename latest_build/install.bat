917784b483795c65f63432435f6201b6ba550c1b
oxyflour <a6905688@gmail.com>
2013-11-24 17:40:51 +0800

    [fix] spell error in install.bat

--- latest_build/install.bat
+++ latest_build/install.bat
@@ -1,4 +1,4 @@
 @cd /d %~dp0
-@PATH=%WINDOR%\Microsoft.NET\Framework\v4.0.30319;%PATH%
+@PATH=%WINDIR%\Microsoft.NET\Framework\v4.0.30319;%PATH%
 regasm /codebase /tlb:SaiHooker.tlb SaiHooker.dll /verbose %1
 pause
